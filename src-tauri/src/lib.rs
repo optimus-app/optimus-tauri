c43a231ffa2d4c62a2da62d175b5687fe0fbb1f1
Victor Chui <biscitplaymc@gmail.com>
2025-01-24 02:55:53 +0800

    feat: button to spawn new window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,15 @@
 use tauri::Manager;
+#[tauri::command]
+async fn create_window(app: tauri::AppHandle) {
+    // Not able to create more than one new window
+    // The second argument in builder could not be a variable or something (not sure why)
+    // In Rust, not sure how global variables are created as well -.-
+    let webview_url = tauri::WebviewUrl::App("index.html".into());
+    let window = tauri::WebviewWindowBuilder::new(&app, "first", webview_url.clone())
+                .title("First")
+                .build()
+                .unwrap();
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -6,10 +17,18 @@ pub fn run() {
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_websocket::init())
         .setup(|app| {
+            let webview_url = tauri::WebviewUrl::App("index.html".into());
             #[cfg(debug_assertions)]
             app.get_webview_window("main").unwrap().open_devtools(); 
+            // tauri::WebviewWindowBuilder::new(app, "first", webview_url.clone())
+            //     .title("First")
+            //     .build()?;
+            // tauri::WebviewWindowBuilder::new(app, "second", webview_url)
+            //     .title("Second")
+            //     .build()?;
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![create_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
 "use client";
 import { useState, useCallback, useEffect } from "react";
-import { useTheme } from "next-themes";
-import WebSocket from "@tauri-apps/plugin-websocket";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { fetch } from "@tauri-apps/plugin-http";
-import { info } from "@tauri-apps/plugin-log";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Icons } from "@/components/icons";
 import WebSocketManager from "./utils/WebSocketManager";
 import HTTPRequestManager, { Methods } from "./utils/HTTPRequestManager";
+import { invoke } from "@tauri-apps/api/core";
 
 export default function Home() {
-    const [message, setMessage] = useState("Hello");
+    const [message, setMessage] = useState("Click to spawn window");
     const [connected, setConnected] = useState("Click to connect");
 
     const wsManager = WebSocketManager.getInstance();
@@ -32,6 +25,10 @@ export default function Home() {
         setConnected("Connected!");
     }, [wsManager]);
 
+    const createWindow = async () => {
+        invoke("create_window");
+    };
+
     const fetchData = async () => {
         const data = await httpManager
             .handleRequest("chat/chatRoom/user1", Methods.GET)
@@ -43,7 +40,7 @@ export default function Home() {
     return (
         <>
             <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <Button onClick={fetchData}>{message}</Button>
+                <Button onClick={createWindow}>{message}</Button>
                 <Button onClick={wsInit}>{connected}</Button>
             </div>
         </>
