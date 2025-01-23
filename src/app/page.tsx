29c30246c435477f0137878815902445fa82c80f
Victor Chui <biscitplaymc@gmail.com>
2025-01-23 20:53:13 +0800

    feat: replicated sample shadcn layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,12 @@ import { useState, useCallback, useEffect } from "react";
 import { useTheme } from "next-themes";
 import WebSocket from "@tauri-apps/plugin-websocket";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { fetch } from "@tauri-apps/plugin-http";
 import { info } from "@tauri-apps/plugin-log";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Moon, MoonIcon, Sun } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Icons } from "@/components/icons";
 
 export default function Home() {
     const { setTheme } = useTheme();
@@ -66,28 +63,80 @@ export default function Home() {
         setMessage(roomTitle);
     };
 
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    async function onSubmit(event: React.SyntheticEvent) {
+        event.preventDefault();
+        setIsLoading(true);
+
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+    }
+
     return (
         <>
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="outline" size="icon">
-                        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                        <span className="sr-only">Toggle theme</span>
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                    <DropdownMenuItem onClick={() => setTheme("light")}>
-                        Light
-                    </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => setTheme("dark")}>
-                        Dark
-                    </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => setTheme("system")}>
-                        System
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+            <div className="container relative h-[800px] flex-ol items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+                <Button
+                    variant="ghost"
+                    className="absolute right-4 top-4 md:right-8 md:top-8"
+                >
+                    Login
+                </Button>
+                <div className="lg:p-8 text-white">
+                    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                        <div className="flex flex-col space-y-2 text-center">
+                            <h1 className="text-2xl font-semibold tracking-tight text-white">
+                                Create an account
+                            </h1>
+                            <p className="text-sm text-muted-foreground text-white">
+                                Enter your email below to create your account
+                            </p>
+                        </div>
+                        <form onSubmit={onSubmit}>
+                            <div className="grid gap-2">
+                                <div className="grid gap-1">
+                                    <Label className="sr-only" htmlFor="email">
+                                        Email
+                                    </Label>
+                                    <Input
+                                        id="email"
+                                        placeholder="name@example.com"
+                                        type="email"
+                                        autoCapitalize="none"
+                                        autoComplete="email"
+                                        autoCorrect="off"
+                                        disabled={isLoading}
+                                    />
+                                </div>
+                                <Button disabled={isLoading}>
+                                    {isLoading && (
+                                        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                                    )}
+                                    Sign In with Email
+                                </Button>
+                            </div>
+                        </form>
+                        <p className="px-8 text-center text-sm text-muted-foreground">
+                            By clicking continue, you agree to our{" "}
+                            <Link
+                                href="/terms"
+                                className="underline underline-offset-4 hover:text-primary"
+                            >
+                                Terms of Service
+                            </Link>{" "}
+                            and{" "}
+                            <Link
+                                href="/privacy"
+                                className="underline underline-offset-4 hover:text-primary"
+                            >
+                                Privacy Policy
+                            </Link>
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
